beff7de3ffa4f90a04da5d624fa1c60096be73dc
Stephen Nielson <nie15023@byui.edu>
2016-04-29 10:25:10 -0600

    Checkstyle fixes for test class.

--- TopicTest.java
+++ TopicTest.java
@@ -8,9 +8,13 @@ public class TopicTest {
   public static void main(String[] args) {
     Topic topic = new Topic("Jesus", Arrays.asList("Savior", "Christ", "Charity", "Bread of Life"));
     
-    Assert.assertTrue("Content should have topic", topic.isInContent("Christ is the one we should turn to."));
-    Assert.assertTrue("Content should have topic with multiple words", topic.isInContent("The Bread of Life is source of all happiness."));
-    Assert.assertTrue("Content should have not have topic with partial words", !topic.isInContent("The Bread of Water is not the source of all happiness."));
-    Assert.assertTrue("Satan should not be in topic", !topic.isInContent("Satan is not the one we should turn to"));
+    Assert.assertTrue("Content should have topic", 
+        topic.isInContent("Christ is the one we should turn to."));
+    Assert.assertTrue("Content should have topic with multiple words", 
+        topic.isInContent("The Bread of Life is source of all happiness."));
+    Assert.assertTrue("Content should have not have topic with partial words", 
+        !topic.isInContent("The Bread of Water is not the source of all happiness."));
+    Assert.assertTrue("Satan should not be in topic", 
+        !topic.isInContent("Satan is not the one we should turn to"));
   }
 }
